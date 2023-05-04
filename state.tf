1ebdd54dbf018b9ed1172faca65586d71466d217
Nishant Shete <61707973+Nishant5050@users.noreply.github.com>
2023-05-04 12:37:15 +0530

    Update state.tf

--- state.tf
+++ state.tf
@@ -2,7 +2,7 @@ terraform {
  backend "s3" {
    bucket         = "test-lambda"
    key            = "state/terraform.tfstate"
-   region         = ap-south-1
+   region         = "ap-south-1"
    encrypt        = true
    kms_key_id     = "alias/terraform-bucket-key"
  }
