491263d5c9fd1adb511e8997212b3ea2bd360d0e
Nishant Shete <61707973+Nishant5050@users.noreply.github.com>
2023-05-04 13:11:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,33 +100,6 @@ resource "aws_iam_role_policy_attachment" "iam_role_policy_attachment_lambda_vpc
 
 
 
-resource "aws_s3_bucket" "terraform-state" {
- bucket = "nishant-bucket-9999"
- acl    = "private"
-
- versioning {
-   enabled = true
- }
-
- server_side_encryption_configuration {
-   rule {
-     apply_server_side_encryption_by_default {
-       kms_master_key_id = aws_kms_key.terraform-bucket-key.arn
-       sse_algorithm     = "aws:kms"
-     }
-   }
- }
-}
-
-resource "aws_s3_bucket_public_access_block" "block" {
- bucket = aws_s3_bucket.terraform-state.id
-
- block_public_acls       = true
- block_public_policy     = true
- ignore_public_acls      = true
- restrict_public_buckets = true
-}
-
 
 
 
