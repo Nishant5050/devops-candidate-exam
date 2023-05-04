48be1695fb6bd3594ab4827f5b1e09a354edb250
Nishant Shete <61707973+Nishant5050@users.noreply.github.com>
2023-05-04 13:01:29 +0530

    Update state.tf

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
  backend "s3" {
-   bucket         = "test-lambda-99"
+   bucket         = "nishant-bucket-9999"
    key            = "state/terraform.tfstate"
    region         = "ap-south-1"
    encrypt        = true
